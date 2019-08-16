0ae7cd4d0d7a3c8528feaee4434f920e53f2d6a7
tomoharu-ishibashi <biz.double.moon84@gmail.com>
2019-08-16 17:49:42 +0900

    migrationとseeder作成

---
+++ database/migrations/2019_08_16_082925_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100)->comment('タイトル');
+            $table->string('description', 255)->comment('説明');
+            $table->date('due_date')->comment('期限');
+            $table->char('status', 1)->comment('ステータス');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
